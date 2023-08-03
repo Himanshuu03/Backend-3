6e47c87c27a8f7da70e01c07fe78606f6424325e
Himanshuu03 <himanshujangra2403@gmail.com>
2023-08-03 20:57:10 +0530

    get-route

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -1,6 +1,6 @@
 const Todo = require("../models/todoModel");
 
-const getTodo =async(req,res)=>{
+exports.getTodo =async(req,res)=>{
     try {
         const response  = await Todo.find({});
         res.status(200).json({
@@ -17,4 +17,28 @@ const getTodo =async(req,res)=>{
     }
 }
 
-module.exports = getTodo;
+exports.getTodoById =async(req,res)=>{
+    try{
+        const id =  req.params.id;
+        const todo = await Todo.findById({_id: id});
+        if(!todo){
+            return(
+                res.status(404).json({
+                    success:false,
+                    message:"id not found"
+                })
+            )
+        }
+        res.status(200).json({
+            success:true,
+            data:todo,
+            message:"Entire todo data",
+        })
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            error:err.message,
+            message:"error",
+        })
+    }
+}
--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,11 @@
 const createTodo = require("../controllers/createTodo");
-const getTodo = require("../controllers/getTodo");
+const {getTodo,getTodoById} = require("../controllers/getTodo");
 
 const express = require("express");
 const router = express.Router();
 
 router.post("/createTodo",createTodo);
 router.get("/getTodos",getTodo);
+router.get("/getTodo/:id",getTodoById);
 
 module.exports = router;
